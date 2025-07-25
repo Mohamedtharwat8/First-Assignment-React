fbb43127dc25dc000998ffb5a379a89153363339
Mohamed Tharwat <Mohamedtharwat8>
2025-07-25 19:21:34 +0300

    Add initial project structure with components, styles, and assets
    - Created SVG assets for download and Vite logos.
    - Added main application styles in App.css.
    - Implemented main App component with Navbar, Home, and Footer components.
    - Created About and Contact components as placeholders.
    - Developed Footer component with welcome message.
    - Built Home component with introductory content and image.
    - Established Navbar component with navigation links and dropdown.
    - Set up main entry point in main.jsx with Bootstrap and FontAwesome integration.
    - Configured Vite for project build.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default   function Footer() {
+    return (
+        <>
+               <div className="container-fluid bg-danger">
+                <h1>Welcome to the Home Page</h1>
+                <p>This is where you can find the latest updates and features.</p>
+            </div>
+        </>
+    )
+}
+
+
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default   function Home() {
+    return (
+        <>
+            <div className="container-fluid bg-warning">
+                <img src="/public/images/avataaars.svg" alt="" />
+                <h1>start Framework</h1>
+                <p>Graphic Artist - Web Designer - Illustrator</p>
+            </div>
+        </>
+    )
+}
+
+
